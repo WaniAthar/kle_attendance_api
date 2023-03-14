8bb1ce45c4465cd9e176389e66a94316d944e3bb
Athar Wani <83573286+WaniAthar@users.noreply.github.com>
2023-03-14 23:16:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def handleAttendanceData(username, password):
         return jsonify({"error": "Invalid username or password"})
     
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False,host='0.0.0.0')
